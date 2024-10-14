7d842227992f7e759a1dfc451079d4e80dd37cc7
DanielBomfim22 <danielufc131@gmail.com>
2024-10-14 12:03:42 -0300

    a

--- back-end/setup/__init__.py
+++ back-end/setup/__init__.py
@@ -4,7 +4,10 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 # Permitir CORS para todas as origens (você pode especificar origens se necessário)
-CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://lista-de-tarefas-react-js-seven.vercel.app/"]}})
+CORS(app, resources={r"/*": {
+    "origins": ["http://localhost:3000", "https://lista-de-tarefas-react-js-seven.vercel.app"],
+    "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+}})
 # no caso coloquei o localhost do meu front-end, e a url da hospedagem dele
 
 from setup.controllers import default
